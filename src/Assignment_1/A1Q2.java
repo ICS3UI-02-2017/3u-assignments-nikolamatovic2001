984c87d4ade7d0a8650efa82de9318ec904807e8
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-02-12 11:20:42 -0500

    Finished A1Q1

--- src/Assignment_1/A1Q2.java
+++ src/Assignment_1/A1Q2.java
@@ -4,6 +4,11 @@
  */
 package Assignment_1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /**
  *
  * @author maton0870
@@ -14,6 +19,20 @@ public class A1Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Create a City for the Robot
+        City nm = new City();
+        
+        // put a robot in nm
+        RobotSE rob = new RobotSE(nm, 1, 2, Direction.SOUTH);
+        
+        // create a wall in the city
+        new Wall(nm, 1, 2, Direction.NORTH);
+        new Wall(nm, 1, 2, Direction.EAST);
+        new Wall(nm, 1, 2, Direction.SOUTH);
+        new Wall(nm, 1, 1, Direction.NORTH);
+        new Wall(nm, 1, 1, Direction.WEST);
+        new Wall(nm, 2, 1, Direction.WEST);
+        new Wall(nm, 2, 1, Direction.SOUTH);
     }
 }
