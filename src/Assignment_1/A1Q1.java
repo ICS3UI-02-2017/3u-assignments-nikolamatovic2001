a0f8658612730a92c8622d4b68fd6bf6461146e6
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-02-12 11:14:38 -0500

    Finished A1Q1

--- src/Assignment_1/A1Q1.java
+++ src/Assignment_1/A1Q1.java
@@ -8,6 +8,7 @@ import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
 import becker.robots.RobotSE;
+import becker.robots.Wall;
 
 /**
  *
@@ -25,5 +26,42 @@ public class A1Q1 {
         
         // Create a robot in the city
         RobotSE rob = new RobotSE(nm, 0, 2, Direction.WEST);
+        
+        // Create the box as shown in the assignment in classroom
+        new Wall(nm, 1, 1, Direction.WEST);
+        new Wall(nm, 2, 1, Direction.SOUTH);
+        new Wall(nm, 1, 2, Direction.NORTH);
+        new Wall(nm, 1, 1, Direction.NORTH);
+        new Wall(nm, 1, 2, Direction.EAST);
+        new Wall(nm, 2, 2, Direction.EAST);
+        new Wall(nm, 2, 1, Direction.WEST);
+        new Wall(nm, 2, 2, Direction.SOUTH);
+        
+        // move the robot forwards two spaces
+        rob.move(2);
+        
+        // turn the robot left once
+        rob.turnLeft();
+        
+        // move forward three spaces
+        rob.move(3);
+        
+        // turn left once
+        rob.turnLeft();
+        
+         // move forward three spaces
+        rob.move(3);
+        
+        // turn left once
+        rob.turnLeft();
+        
+        // move forward three spaces
+        rob.move(3);
+        
+        // turn left once
+        rob.turnLeft();
+        
+        // move forward one space
+        rob.move();
     }
 }
