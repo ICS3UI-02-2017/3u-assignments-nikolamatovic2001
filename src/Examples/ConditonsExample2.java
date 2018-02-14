46cb3974705d3df1eb4f38701689840dd7fa26ac
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-02-14 11:16:13 -0500

    Finished A1Q1 Finished A1Q2 Finished A1Q3 Finished A1Q4 Finished A1Q5

---
+++ src/Examples/ConditonsExample2.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examples;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author maton0870
+ */
+public class ConditonsExample2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Create a City for the Robot
+        City nm = new City();
+        
+        // put a robot in nm
+         RobotSE rob = new RobotSE(nm, 2, 0, Direction.EAST);
+
+        new Wall(nm, 2, 5, Direction.EAST);
+        
+        new Thing (nm, 2, 2, Direction.EAST);
+        new Thing (nm, 2, 4, Direction.EAST);
+
+         while(rob.frontIsClear()){
+         rob.move();
+         }  
+         rob.turnRight();
+       
+          if(rob.canPickThing()){
+           rob.pickThing();
+        }
+          if(rob.countThingsInBackpack() == 1){
+              rob.move();
+          }else if (rob.countThingsInBackpack() == 2){
+              rob.move(2);
+          }else if (rob.countThingsInBackpack() == 0) {
+              rob.turnLeft();
+              rob.move();
+          }
+    }
+}
