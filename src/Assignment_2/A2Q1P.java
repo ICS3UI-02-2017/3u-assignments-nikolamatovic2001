db8aabc29a9783e99fda906f5cdc72132d6f0bdf
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-02-15 11:20:31 -0500

    Finished A2Q1P

--- src/Assignment_2/A2Q1P.java
+++ src/Assignment_2/A2Q1P.java
@@ -6,7 +6,9 @@ package Assignment_2;
 
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.IPredicate;
 import becker.robots.RobotSE;
+import becker.robots.Thing;
 
 /**
  *
@@ -23,7 +25,42 @@ public class A2Q1P {
         City nm = new City ();
         
         //Create a robot in the city
-        RobotSE rob = new RobotSE(nm, 2, 3, Direction.NORTH);
+        RobotSE rob = new RobotSE(nm, 1, 1, Direction.EAST);
         
-    }
-}
+        //Creating 10 things in a line
+        new Thing(nm, 1, 2, Direction.EAST);
+        new Thing(nm, 1, 3, Direction.EAST);
+        new Thing(nm, 1, 4, Direction.EAST);
+        new Thing(nm, 1, 5, Direction.EAST);
+        new Thing(nm, 1, 6, Direction.EAST);
+        new Thing(nm, 1, 7, Direction.EAST);
+        new Thing(nm, 1, 8, Direction.EAST);
+        new Thing(nm, 1, 9, Direction.EAST);
+        new Thing(nm, 1, 10, Direction.EAST);
+        new Thing(nm, 1, 11, Direction.EAST);
+         
+        
+        
+        //moves rob forward once
+        rob.move();
+       
+        // rob will check to see if he has less the 7 things in his backpack
+        while(rob.countThingsInBackpack() < 7){
+      
+        // rob picks up thing and moves forward one
+        rob.pickThing();
+        rob.move();
+       
+        // rob will check to see if he has less the 7 things in his backpack
+        if(rob.countThingsInBackpack() == 7){
+            
+        //rob moves forward 3
+        rob.move(3);
+         
+             }
+           }
+         }
+       }
+    
+      
+    
