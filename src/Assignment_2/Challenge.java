970f29c34ad21f59321a0b7ac7eb17a40f96a18d
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-02-20 11:14:55 -0500

    Finished A2Q4P

---
+++ src/Assignment_2/Challenge.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_2;
+
+/**
+ *
+ * @author maton0870
+ */
+public class Challenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
