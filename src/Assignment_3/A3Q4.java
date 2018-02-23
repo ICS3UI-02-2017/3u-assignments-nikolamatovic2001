cbb8b8926fe957ce63571862616d3a3c7eaed2af
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-02-23 11:15:04 -0500

    Finished A3Q1 Finished A3Q2 Finished A3Q3

--- src/Assignment_3/A3Q4.java
+++ src/Assignment_3/A3Q4.java
@@ -4,6 +4,11 @@
  */
 package Assignment_3;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /**
  *
  * @author maton0870
@@ -15,5 +20,47 @@ public class A3Q4 {
      */
     public static void main(String[] args) {
         
+        // Create a City for rob
+        City nm = new City();
+        
+        // put rob in nm
+        RobotSE rob = new RobotSE(nm, 3, 3, Direction.SOUTH);
+                
+        //creating 4 blocks in the city
+        new Wall (nm, 1, 1, Direction.WEST);
+        new Wall (nm, 1, 1, Direction.NORTH);
+        new Wall (nm, 1, 2, Direction.EAST);
+        new Wall (nm, 1, 2, Direction.NORTH);
+        new Wall (nm, 2, 1, Direction.WEST);
+        new Wall (nm, 2, 1, Direction.SOUTH);
+        new Wall (nm, 2, 2, Direction.SOUTH);
+        new Wall (nm, 2, 2, Direction.EAST);
+        
+        new Wall (nm, 1, 4, Direction.WEST);
+        new Wall (nm, 1, 4, Direction.NORTH);
+        new Wall (nm, 1, 5, Direction.EAST);
+        new Wall (nm, 1, 5, Direction.NORTH);
+        new Wall (nm, 2, 4, Direction.WEST);
+        new Wall (nm, 2, 4, Direction.SOUTH);
+        new Wall (nm, 2, 5, Direction.SOUTH);
+        new Wall (nm, 2, 5, Direction.EAST);
+        
+        new Wall (nm, 4, 1, Direction.WEST);
+        new Wall (nm, 4, 1, Direction.NORTH);
+        new Wall (nm, 4, 2, Direction.EAST);
+        new Wall (nm, 4, 2, Direction.NORTH);
+        new Wall (nm, 5, 1, Direction.WEST);
+        new Wall (nm, 5, 1, Direction.SOUTH);
+        new Wall (nm, 5, 2, Direction.SOUTH);
+        new Wall (nm, 5, 2, Direction.EAST);
+        
+        new Wall (nm, 4, 4, Direction.WEST);
+        new Wall (nm, 4, 4, Direction.NORTH);
+        new Wall (nm, 4, 5, Direction.EAST);
+        new Wall (nm, 4, 5, Direction.NORTH);
+        new Wall (nm, 5, 4, Direction.WEST);
+        new Wall (nm, 5, 4, Direction.SOUTH);
+        new Wall (nm, 5, 5, Direction.SOUTH);
+        new Wall (nm, 5, 5, Direction.EAST);
     }
 }
