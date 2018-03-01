ba9a8cecfcb99d11b33ea6b47a50994f3d7e2126
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-03-01 10:47:12 -0500

    Finished A4Q2

---
+++ src/Assignment_4/A4Q3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maton0870
+ */
+public class A4Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Please enter in 4 numbers on seperate lines:");
+        Scanner input = new Scanner(System.in);
+        String one = input.nextLine();
+    }
+}
