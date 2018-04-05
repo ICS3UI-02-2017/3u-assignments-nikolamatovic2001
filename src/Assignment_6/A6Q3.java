81f05e161c38877f116ca21e975c7a6cb6c73ff8
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-04-05 10:32:25 -0400

    Finished A6Q3

---
+++ src/Assignment_6/A6Q3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maton0870
+ */
+public class A6Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+        // Places two integers into the array
+        int[] sort = new int[2];
+        for (int i = 0; i < sort.length; i++) {
+            int num = input.nextInt();
+            sort[i] = num;
+        }
+        // Sorts the numbers in ascending order
+        for (int i = 0; i < sort.length; i++) {
+            if (sort[i] < sort[i + 1]) {
+                System.out.println("The integers in ascending order are: " + sort[i] + " " + sort[i + 1]);
+            } else {
+                System.out.println("The integers in ascending order are: " + sort[i + 1] + " " + sort[i]);
+            }
+            break;
+        }
+    }
+}
