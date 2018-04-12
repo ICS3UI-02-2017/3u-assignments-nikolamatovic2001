404246c7ba80a1e8055aea443e8755203159ec40
maton0870 <maton0870@HRH-IC0027327.wrdsb.ca>
2018-04-12 10:29:00 -0400

    Finished A6Q4

--- src/Assignment_6/A6Q4.java
+++ src/Assignment_6/A6Q4.java
@@ -5,6 +5,7 @@
 package Assignment_6;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  *
@@ -17,23 +18,29 @@ public class A6Q4 {
      */
     public static void main(String[] args) {
 
+        //Asks the user to input the numbers
         Scanner input = new Scanner(System.in);
         System.out.println("Please input the 10 marks down below; ");
 
+        //Creates a 10 int array
         int[] sort = new int[10];
 
-        for (int x = 0; x < sort.length - 1; x++) {
+        //Puts numbers into the array
+        for (int x = 0; x < sort.length; x++) {
             int num = input.nextInt();
             sort[x] = num;
-
+        }
+        //Sorts the numbers from smallest to biggest
+        for (int x = 0; x < sort.length - 1; x++) {
             for (int y = x + 1; y < sort.length; y++) {
                 if (sort[x] > sort[y]) {
                     int store = sort[x];
                     sort[x] = sort[y];
                     sort[y] = store;
                 }
-                System.out.print(sort[x]);
             }
         }
+        //Outputs the numbers 
+        System.out.print(Arrays.toString(sort));
     }
 }
